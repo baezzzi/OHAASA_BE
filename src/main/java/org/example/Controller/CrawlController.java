99622570c7fd95457463ca892b5752ed454a66d5
jiyeon <hannee233@dankook.ac.kr>
2025-06-11 20:13:49 +0900

    [DONE] RestController update

--- src/main/java/org/example/Controller/CrawlController.java
+++ src/main/java/org/example/Controller/CrawlController.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/crawl")
 public class CrawlController {
     private final CrawlService crawlService;
