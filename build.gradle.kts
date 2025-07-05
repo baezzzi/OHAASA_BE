a63ca9f3869f0c918560ca8e1dcd1386b0d38713
jiyeon <hannee233@dankook.ac.kr>
2025-07-06 01:06:46 +0900

    gradle add

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,19 +15,19 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 
     // Lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation ("com.google.firebase:firebase-admin:9.5.0")
     implementation ("org.jsoup:jsoup:1.17.2")
     implementation("org.seleniumhq.selenium:selenium-java:4.17.0")
-    implementation ("org.springframework.boot:spring-boot-starter-webflux")
     runtimeOnly ("io.netty:netty-resolver-dns-native-macos:4.1.104.Final:osx-aarch_64")
     implementation ("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.google.firebase:firebase-admin:9.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+
 }
 
 
