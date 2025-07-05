1a7c1bfff37f7211782c2d7f30979b1c458f0f95
jiyeon <hannee233@dankook.ac.kr>
2025-07-05 22:57:56 +0900

    mysql file remove

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,9 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
     // Lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-
-    implementation("mysql:mysql-connector-java:8.0.33")
 
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation ("com.google.firebase:firebase-admin:9.5.0")
@@ -33,14 +28,6 @@ dependencies {
     implementation("org.seleniumhq.selenium:selenium-java:4.17.0")
     implementation ("org.springframework.boot:spring-boot-starter-webflux")
     runtimeOnly ("io.netty:netty-resolver-dns-native-macos:4.1.104.Final:osx-aarch_64")
-
-//    implementation ("com.google.cloud:spring-cloud-gcp-starter-firestore:4.8.2")
-//    implementation ("org.springframework.cloud:spring-cloud-gcp-starter-data-firestore")
-
-
-
-
-
 }
 
 
