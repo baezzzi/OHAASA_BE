2038adbd45288e5ec7976fdb39465ee327232e45
jiyeon <hannee233@dankook.ac.kr>
2025-07-18 15:05:00 +0900

    test

--- src/main/java/org/example/Component/HoroscopeSaveScheduler.java
+++ src/main/java/org/example/Component/HoroscopeSaveScheduler.java
@@ -22,7 +22,7 @@ public class HoroscopeSaveScheduler {
         System.out.println("번역 완료 및 결과 저장");
     }
 
-    @Scheduled(cron = "0 6 15 ? * MON-FRI", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 20 15 ? * MON-FRI", zone = "Asia/Seoul")
     public void saveScheduler2() throws IOException, InterruptedException, ExecutionException {
         crawlService.translateSaveAndApply();
         System.out.println("번역 완료 및 결과 저장");
